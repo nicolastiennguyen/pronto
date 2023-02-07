9c0dc2ab2d178b3ca7441864eecf46586cf85c24
Nick <nicolastiennguyen@gmail.com>
2023-02-06 23:11:31 -0800

    websockets are talking to each other

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import WebSocket, { WebSocketServer } from 'ws'
 
 function App() {
+  const wss = new WebSocket({port:8080})
+
+
+  useEffect(() => {
+    wss.on('connection', (ws) => {
+      console.log('New client connected!')
+
+      ws.send(JSON.stringify('Welcome!'))
+      ws.on('message', (message) => {
+        ws.send(`Your message: ${message}`)
+      })
+    })
+  }, [])
 
   return (
     <div className="App">
---
+++ src/server.js
@@ -0,0 +1,18 @@
+import WebSocket, { WebSocketServer } from 'ws'
+
+const wss = new WebSocketServer({port:8080})
+
+wss.on('connection', (ws) => {
+  console.log('A new client connected!')
+  ws.send('Welcome new client!')
+
+  ws.on('message', (data) => {
+    console.log(`received a message: ${data}`)
+    // ws.send(`Your message: ${data}`)
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data)
+      }
+    })
+  })
+})
